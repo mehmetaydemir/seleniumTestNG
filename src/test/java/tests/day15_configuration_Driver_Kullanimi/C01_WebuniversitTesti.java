5914a44e5f6c22d62c28cd15892ba39bb13acc4b
mehmetaydemir <mehmetaydemir032@gmail.com>
2023-12-16 20:55:23 +0300

    first

---
+++ src/test/java/tests/day15_configuration_Driver_Kullanimi/C01_WebuniversitTesti.java
@@ -0,0 +1,14 @@
+package tests.day15_configuration_Driver_Kullanimi;
+
+import org.testng.annotations.Test;
+
+public class C01_WebuniversitTesti {
+
+    @Test
+    public  void negatifLoginTesti(){
+
+    }
+
+
+
+}
